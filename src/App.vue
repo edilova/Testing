0484c4e922d91f02a75e58d60a795a9f75a5323a
Aruzhan Edilova <avolide@gmail.com>
2019-06-18 19:39:59 +0600

    App.vue refactor

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <!--<ProductList></ProductList>-->
     <router-view/>
   </div>
 </template>
 
 <script>
-  import ProductList from './components/ProductList'
 
 export default {
     name: 'app',
-    // components:{
-    //   ProductList
-    // }
 }
 </script>
 
@@ -23,5 +18,4 @@ export default {
   body{
     background-color: #fff;
   }
-
 </style>
